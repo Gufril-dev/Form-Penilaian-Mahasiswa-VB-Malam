2cd638a70c0b291088d844882f1ae165b0742ceb
Gufril-dev <85840346+Gufril-dev@users.noreply.github.com>
2022-11-06 18:14:52 +0700

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,390 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Txtname = New System.Windows.Forms.TextBox()
+        Me.Txtnpm = New System.Windows.Forms.TextBox()
+        Me.Txttaskscore = New System.Windows.Forms.TextBox()
+        Me.Txtutsscore = New System.Windows.Forms.TextBox()
+        Me.Txtuasscore = New System.Windows.Forms.TextBox()
+        Me.Txtsks = New System.Windows.Forms.TextBox()
+        Me.Txtipk = New System.Windows.Forms.TextBox()
+        Me.Txtips = New System.Windows.Forms.TextBox()
+        Me.Txtpresence = New System.Windows.Forms.TextBox()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.ComboBoxmajor = New System.Windows.Forms.ComboBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Txttotalscore = New System.Windows.Forms.TextBox()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.Txttotalsemester = New System.Windows.Forms.TextBox()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.Txtgrade = New System.Windows.Forms.TextBox()
+        Me.Label15 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(62, 99)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(55, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "NAME"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(62, 151)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(43, 20)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "NPM"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(62, 205)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(65, 20)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "MAJOR"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(441, 99)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(89, 20)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "Task Score"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(441, 151)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(87, 20)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "UTS Score"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(443, 205)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(89, 20)
+        Me.Label6.TabIndex = 3
+        Me.Label6.Text = "UAS Score"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(64, 264)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(41, 20)
+        Me.Label7.TabIndex = 6
+        Me.Label7.Text = "SKS"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(443, 264)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(76, 20)
+        Me.Label8.TabIndex = 7
+        Me.Label8.Text = "Presence"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.Location = New System.Drawing.Point(338, 493)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(41, 22)
+        Me.Label9.TabIndex = 8
+        Me.Label9.Text = "IPS"
+        '
+        'Txtname
+        '
+        Me.Txtname.Location = New System.Drawing.Point(160, 96)
+        Me.Txtname.Name = "Txtname"
+        Me.Txtname.Size = New System.Drawing.Size(213, 26)
+        Me.Txtname.TabIndex = 9
+        '
+        'Txtnpm
+        '
+        Me.Txtnpm.Location = New System.Drawing.Point(160, 148)
+        Me.Txtnpm.Name = "Txtnpm"
+        Me.Txtnpm.Size = New System.Drawing.Size(213, 26)
+        Me.Txtnpm.TabIndex = 10
+        '
+        'Txttaskscore
+        '
+        Me.Txttaskscore.Location = New System.Drawing.Point(547, 96)
+        Me.Txttaskscore.Name = "Txttaskscore"
+        Me.Txttaskscore.Size = New System.Drawing.Size(99, 26)
+        Me.Txttaskscore.TabIndex = 11
+        '
+        'Txtutsscore
+        '
+        Me.Txtutsscore.Location = New System.Drawing.Point(549, 145)
+        Me.Txtutsscore.Name = "Txtutsscore"
+        Me.Txtutsscore.Size = New System.Drawing.Size(97, 26)
+        Me.Txtutsscore.TabIndex = 12
+        '
+        'Txtuasscore
+        '
+        Me.Txtuasscore.Location = New System.Drawing.Point(549, 199)
+        Me.Txtuasscore.Name = "Txtuasscore"
+        Me.Txtuasscore.Size = New System.Drawing.Size(97, 26)
+        Me.Txtuasscore.TabIndex = 13
+        '
+        'Txtsks
+        '
+        Me.Txtsks.Location = New System.Drawing.Point(160, 258)
+        Me.Txtsks.Name = "Txtsks"
+        Me.Txtsks.Size = New System.Drawing.Size(97, 26)
+        Me.Txtsks.TabIndex = 14
+        '
+        'Txtipk
+        '
+        Me.Txtipk.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.Txtipk.Location = New System.Drawing.Point(610, 492)
+        Me.Txtipk.Name = "Txtipk"
+        Me.Txtipk.Size = New System.Drawing.Size(97, 26)
+        Me.Txtipk.TabIndex = 15
+        '
+        'Txtips
+        '
+        Me.Txtips.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.Txtips.Location = New System.Drawing.Point(389, 493)
+        Me.Txtips.Name = "Txtips"
+        Me.Txtips.Size = New System.Drawing.Size(97, 26)
+        Me.Txtips.TabIndex = 16
+        '
+        'Txtpresence
+        '
+        Me.Txtpresence.Location = New System.Drawing.Point(549, 258)
+        Me.Txtpresence.Name = "Txtpresence"
+        Me.Txtpresence.Size = New System.Drawing.Size(97, 26)
+        Me.Txtpresence.TabIndex = 17
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label10.Location = New System.Drawing.Point(563, 495)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(41, 22)
+        Me.Label10.TabIndex = 18
+        Me.Label10.Text = "IPK"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Font = New System.Drawing.Font("Lucida Bright", 11.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label11.ForeColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.Label11.Location = New System.Drawing.Point(236, 21)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(361, 25)
+        Me.Label11.TabIndex = 19
+        Me.Label11.Text = "STUDENT ASSESSEMENT FORM"
+        '
+        'ComboBoxmajor
+        '
+        Me.ComboBoxmajor.FormattingEnabled = True
+        Me.ComboBoxmajor.Items.AddRange(New Object() {"SISTEM INFORMASI", "SISTEM INFORMASI AKUNTANSI", "AKUNTANSI", "MANAJEMEN"})
+        Me.ComboBoxmajor.Location = New System.Drawing.Point(160, 202)
+        Me.ComboBoxmajor.Name = "ComboBoxmajor"
+        Me.ComboBoxmajor.Size = New System.Drawing.Size(213, 28)
+        Me.ComboBoxmajor.TabIndex = 20
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(196, 413)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(115, 38)
+        Me.Button1.TabIndex = 21
+        Me.Button1.Text = "CLEAR"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(482, 413)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(115, 38)
+        Me.Button2.TabIndex = 22
+        Me.Button2.Text = "COUNT"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Txttotalscore
+        '
+        Me.Txttotalscore.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.Txttotalscore.ForeColor = System.Drawing.SystemColors.MenuText
+        Me.Txttotalscore.Location = New System.Drawing.Point(171, 494)
+        Me.Txttotalscore.Name = "Txttotalscore"
+        Me.Txttotalscore.Size = New System.Drawing.Size(97, 26)
+        Me.Txttotalscore.TabIndex = 24
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label12.Location = New System.Drawing.Point(25, 492)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(124, 25)
+        Me.Label12.TabIndex = 23
+        Me.Label12.Text = "Total Score"
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(62, 317)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(117, 20)
+        Me.Label13.TabIndex = 25
+        Me.Label13.Text = "Total Semester"
+        '
+        'Txttotalsemester
+        '
+        Me.Txttotalsemester.Location = New System.Drawing.Point(196, 317)
+        Me.Txttotalsemester.Name = "Txttotalsemester"
+        Me.Txttotalsemester.Size = New System.Drawing.Size(97, 26)
+        Me.Txttotalsemester.TabIndex = 26
+        '
+        'Label14
+        '
+        Me.Label14.AutoSize = True
+        Me.Label14.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label14.Location = New System.Drawing.Point(236, 577)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(86, 25)
+        Me.Label14.TabIndex = 27
+        Me.Label14.Text = "GRADE"
+        '
+        'Txtgrade
+        '
+        Me.Txtgrade.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.Txtgrade.Location = New System.Drawing.Point(328, 578)
+        Me.Txtgrade.Name = "Txtgrade"
+        Me.Txtgrade.Size = New System.Drawing.Size(69, 26)
+        Me.Txtgrade.TabIndex = 28
+        '
+        'Label15
+        '
+        Me.Label15.AutoSize = True
+        Me.Label15.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label15.Location = New System.Drawing.Point(652, 261)
+        Me.Label15.Name = "Label15"
+        Me.Label15.Size = New System.Drawing.Size(38, 22)
+        Me.Label15.TabIndex = 29
+        Me.Label15.Text = "/16"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(757, 656)
+        Me.Controls.Add(Me.Label15)
+        Me.Controls.Add(Me.Txtgrade)
+        Me.Controls.Add(Me.Label14)
+        Me.Controls.Add(Me.Txttotalsemester)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.Txttotalscore)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ComboBoxmajor)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Txtpresence)
+        Me.Controls.Add(Me.Txtips)
+        Me.Controls.Add(Me.Txtipk)
+        Me.Controls.Add(Me.Txtsks)
+        Me.Controls.Add(Me.Txtuasscore)
+        Me.Controls.Add(Me.Txtutsscore)
+        Me.Controls.Add(Me.Txttaskscore)
+        Me.Controls.Add(Me.Txtnpm)
+        Me.Controls.Add(Me.Txtname)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Txtname As TextBox
+    Friend WithEvents Txtnpm As TextBox
+    Friend WithEvents Txttaskscore As TextBox
+    Friend WithEvents Txtutsscore As TextBox
+    Friend WithEvents Txtuasscore As TextBox
+    Friend WithEvents Txtsks As TextBox
+    Friend WithEvents Txtipk As TextBox
+    Friend WithEvents Txtips As TextBox
+    Friend WithEvents Txtpresence As TextBox
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents ComboBoxmajor As ComboBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Txttotalscore As TextBox
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label13 As Label
+    Friend WithEvents Txttotalsemester As TextBox
+    Friend WithEvents Label14 As Label
+    Friend WithEvents Txtgrade As TextBox
+    Friend WithEvents Label15 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,49 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Txtname.Text = ""
+        Txtnpm.Text = ""
+        ComboBoxmajor.Text = ""
+        Txttaskscore.Text = " "
+        Txttaskscore.Text = ""
+        Txtutsscore.Text = ""
+        Txtuasscore.Text = ""
+        Txtpresence.Text = ""
+        Txtsks.Text = ""
+        Txtips.Text = ""
+        Txtipk.Text = ""
+        Txttotalscore.Text = ""
+        Txttotalsemester.Text = ""
+        Txtgrade.Text = ""
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim label1 = Txtname.Text
+        Dim label2 = Txtnpm.Text
+        Dim label3 = ComboBoxmajor.Text
+        Dim label4 = Txtsks.Text
+        Dim label5 = Txttaskscore.Text
+        Dim label6 = Txtutsscore.Text
+        Dim label7 = Txtuasscore.Text
+        Dim label8 = Txtpresence.Text
+        Dim label9 = Txtips.Text
+        Dim label10 = Txtipk.Text
+        Dim label12 = Txttotalscore.Text
+        Dim label13 = Txttotalsemester.Text
+        Dim label14 = Txtgrade.Text
+
+        Txttotalscore.Text = (((Txtpresence.Text / 16) * 0.1) + Txttaskscore.Text * 0.2 + Txtutsscore.Text * 0.3 + Txtuasscore.Text * 0.4)
+        Txtips.Text = Txttotalscore.Text / Txtsks.Text
+        Txtipk.Text = (Txttotalscore.Text * Txttotalsemester.Text) / (Txtsks.Text * Txttotalsemester.Text)
+
+        If Txtipk.Text >= 4 Then
+            Txtgrade.Text = "A"
+        ElseIf Txtipk.text >= 3 Then
+            Txtgrade.Text = "B"
+        ElseIf Txtipk.text >= 2 Then
+            Txtgrade.Text = "C"
+        ElseIf Txtipk.text >= 1 Then
+            Txtgrade.Text = "D"
+        End If
+    End Sub
+
+End Class
